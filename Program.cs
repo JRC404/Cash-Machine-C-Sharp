22ce3fd805392e0fbf38298cc82381fe6f306bb2
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-03-05 09:08:59 +0000

    C# intro for cash machine.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace csharp_cashMachine
+{
+    class Program
+    {
+        void withdrawl(int bankBalance, int withdrawlAmount)
+        {
+            bankBalance -= withdrawlAmount;
+            Console.WriteLine("My new balance is: {0}", bankBalance);
+        }
+
+        // void checkPin(string userInput, string pinNumber)
+
+        void checkPin()
+        {
+        START:
+            string pinNumber = "1000";
+            string userInput;
+            Console.Write("Please enter your input: ");
+            userInput = Console.ReadLine();
+            // Console.WriteLine(userInput);
+
+            if (pinNumber == userInput)
+            {
+                Console.WriteLine("Woah, you are allowed in.");
+            }
+            else
+            {
+                // Console.WriteLine("Go away.");
+                goto START;
+            }
+            // if userInput is equal to the pinNumber... do something
+            // if it isn't... do something
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program cashMachine = new Program();
+            cashMachine.withdrawl(500, 30);
+            cashMachine.checkPin();
+            // taking user input
+
+
+
+        }
+    }
+}
+
